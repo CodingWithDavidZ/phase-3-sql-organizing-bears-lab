40c4fc64bbb0f413e3dfc10d28f48dd0a4cb0054
David Zombar <davidzombar@gmail.com>
2021-09-15 13:43:03 -0600

    commit tree 7c03e394563cfbf25b662f314aa47e6fe7c42ec2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", null, "M", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", null, "M", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", null, "F", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergent Brown", null, "M", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", null, "F", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", null, "M", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", null, "F", null, null, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, null, "M", null, null, 1);
